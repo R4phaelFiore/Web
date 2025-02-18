62cc3c903602dd17a6a153476fe139a7d5f309d2
Raphael Mendes Fiore <118700992+R4phaelFiore@users.noreply.github.com>
2025-02-18 06:36:17 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("EventBtn");
+
+var iconPython = document.querySelector(".iconPy");
+
+btn.addEventListener("click", function(){
+    index.style.display = 'none';
+
+});
